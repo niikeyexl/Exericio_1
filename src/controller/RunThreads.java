7ee2c075c8ee9b1e4a1fa2a9a60e02071657e412
ecben <ecben@DESKTOP-TIOMS7V>
2020-09-14 20:13:00 -0300

    Update exercicio 1/5

--- src/controller/RunThreads.java
+++ src/controller/RunThreads.java
@@ -2,14 +2,8 @@ package controller;
 
 public class RunThreads extends Thread {
 	
-	private int idThread;
-	
-	public RunThreads(int idThread) {
-		this.idThread = idThread;
-	}
-	
 	@Override
 	public void run() {
-		System.out.println(idThread);
+		System.out.println(getId());
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		for (int threadId = 0; threadId < 5; threadId++) {
-			new RunThreads(threadId).start();
+			new RunThreads().start();
 		}
 
 	}
